b83e70c5665ea6b290d1f549f5b4f080593723d5
Harshita <harshita@Harshitas-MacBook-Air.local>
2026-06-10 15:12:13 +0530

    Add assignment2 code

---
+++ assignment2.cpp
@@ -0,0 +1,76 @@
+//next smaller element
+
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> nextSmaller(vector<int>& nums)
+{
+    int n = nums.size();
+    vector<int> ans(n);
+    stack<int> st;
+
+    for(int i = n - 1; i >= 0; i--)
+    {
+        while(!st.empty() && st.top() >= nums[i])
+        {
+            st.pop();
+        }
+
+        if(st.empty())
+        {
+            ans[i] = -1;
+        }
+        else
+        {
+            ans[i] = st.top();
+        }
+
+        st.push(nums[i]);
+    }
+
+    return ans;
+}
+
+
+
+
+
+//previous smaller element
+
+
+
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> nextSmaller(vector<int>& nums)
+{
+    int n = nums.size();
+    vector<int> ans(n);
+    stack<int> st;
+
+    for(int i = 0; i <n; i++)
+    {
+        while(!st.empty() && st.top() >= nums[i])
+        {
+            st.pop();
+        }
+
+        if(st.empty())
+        {
+            ans[i] = -1;
+        }
+        else
+        {
+            ans[i] = st.top();
+        }
+
+        st.push(nums[i]);
+    }
+
+    return ans;
+}
+
+
+
